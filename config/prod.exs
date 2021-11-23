59ff1ec9417857e4559333ab911e5fc34f5d6d0e
tywhisky <fty221@gmail.com>
2021-11-23 16:53:14 +0800

    feat: new secret

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :os_blog, OsBlogWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "localhost", port: 8080]
 
 # Do not print debug messages in production
 config :logger, level: :info
