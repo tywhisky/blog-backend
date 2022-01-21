2c7b5ed523f7f37b326413341a5c889b30175081
tywhisky <mikoto221@qq.com>
2022-01-21 17:12:48 +0800

    feat: update formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,12 @@
 [
-  import_deps: [:ecto, :phoenix],
+  import_deps: [:ecto, :phoenix, :plug, :absinthe, :ecto_sql],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  subdirectories: ["priv/*/migrations"],
+  locals_without_parens: [
+    oban_dashboard: 1,
+    service: 1,
+    service: 2,
+    redact_field: 2,
+    redact_field: 3
+  ]
 ]
