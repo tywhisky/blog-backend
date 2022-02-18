09e7ec1eebc18d96adb0c93179d1df76c4e7cbb3
tywhisky <mikoto221@qq.com>
2022-02-18 10:47:39 +0800

    feat: add new

--- mix.exs
+++ mix.exs
@@ -65,7 +65,8 @@ defmodule OsBlog.MixProject do
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      s: ["phx.server"]
     ]
   end
 end
